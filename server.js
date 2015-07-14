ce81727ab7ca3b2ce02e5905e8f93220e2efb4a6
Max Weller <git@max-weller.de>
2015-07-14 19:16:00 +0200

    create manifests

---
+++ server.js
@@ -0,0 +1,13 @@
+var express =           require('express')
+    , http =            require('http')
+    , path =            require('path')
+
+    
+var app = module.exports = express();
+app.use(express.static(path.join(__dirname, 'client')));
+
+
+app.set('port', process.env.PORT || 8000);
+http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
