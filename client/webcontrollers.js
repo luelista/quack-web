eec9f97d28efec865d26e9be26247ea990d9dac4
Max Weller <git@max-weller.de>
2015-07-16 03:19:26 +0200

    password change

--- client/webcontrollers.js
+++ client/webcontrollers.js
@@ -14,6 +14,36 @@ angular.module( 'bonfireControllers', [  ] )
     $rootScope.$on('$routeChangeSuccess', function(event, current) {
       update();
     });
+    $scope.notificationsSupported = ('Notification' in window);
+    $scope.notificationsEnabled = (window.localStorage.notificationsEnabled == "true");
+    $scope.$watch('notificationsEnabled', function() {
+      window.localStorage.notificationsEnabled = $scope.notificationsEnabled ? "true" : "";
+    });
+    $scope.toggleNotifications = function() {
+      if ($scope.notificationsEnabled) {
+        $scope.notificationsEnabled = false;
+      } else {
+        Notification.requestPermission(function(permission){
+          $scope.$apply(function() {
+            $scope.notificationsEnabled = true;
+          });
+        });
+      }
+    }
+    $scope.preferences = function() {
+      $location.path("/preferences");
+    }
+    $scope.changePassword = function(ev) {
+      $mdDialog.show({
+        controller: ChangePasswordController,
+        templateUrl: 'partials/change_password.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        onComplete: function(scope, element) {
+          element[0].querySelector('[focus-me]').focus();
+        }
+      })
+    }
     function update() {
       var loc = $scope.currentPage = $location.path();
       $scope.conversations = Jabber.conferences;
@@ -32,36 +62,6 @@ angular.module( 'bonfireControllers', [  ] )
     }
   })
 
-.controller("LoginCtrl",
-  function($scope, $location, $mdToast, Jabber) {
-    $scope.jabber = Jabber;
-
-    $scope.register = function() {
-      window.open(XMPP_REGISTER_FORM, "regform", "width=450,height=300");
-    }
-
-    $scope.login = function() {
-      Jabber.connect()
-      .then(function() {
-        var s = $location.search();
-        if (s.back) {
-          $location.path(s.back).search({});
-        } else {
-          $location.path("/welcome");
-        }
-      }, function(error) {
-        $mdToast.showSimple(error);
-
-      }, function(notice) {
-        $mdToast.showSimple(notice);
-
-      });
-    }
-
-    if (Jabber.jid && Jabber.password && window.localStorage.login) $scope.login();
-
-
-  })
 
 .controller("WelcomeCtrl",
   function($scope, Jabber) {
@@ -77,106 +77,6 @@ angular.module( 'bonfireControllers', [  ] )
       room.bookmark();
     };
   })
-
-.controller("ChatCtrl",
-  function($scope, $routeParams, Jabber, $mdToast) {
-    var id = $scope.jid = $routeParams.chatId;
-    $scope.chat = Jabber.conferences[id];
-    if (!$scope.chat) {
-      $mdToast.showSimple("Unknown conference"); return;
-    }
-    $scope.skipItemCount = $scope.chat.messages.length - 75;
-    $scope.chat.composing = $scope.chat.composing || "";
-    $scope.chat.lastRead = $scope.chat.lastReceived;
-    $scope.chat.persist();
-
-    $scope.sendMessage = function() {
-      console.log($scope.chat.composing);
-      Jabber.client.sendMessage({
-        body: $scope.chat.composing,
-        to: $scope.chat.jid,
-        type: "groupchat"
-      });
-      $scope.chat.composing = "";
-    }
-    $scope.$on("sendmessage", function() {
-      $scope.sendMessage();
-    });
-    $scope.$on("reachedtop", function() {
-      $scope.skipItemCount = Math.max(0, $scope.skipItemCount - 25);
-    });
-
-    $scope.goOnline = function() {
-      Jabber.joinRoom($scope.chat);
-      $scope.chat.autoJoin = true;
-      $scope.chat.bookmark();
-    }
-    $scope.goOffline = function() {
-      $scope.chat.part();
-      $scope.chat.autoJoin = false;
-      $scope.chat.bookmark();
-    }
-    $scope.delete = function() {
-      $scope.chat.unbookmark();
-    }
-    $scope.rename = function() {
-
-    }
-  })
-
-.directive('chatWriteMessage', ['$interval', 'dateFilter', function($interval, dateFilter) {
-
-  function link(scope, element, attrs) {
-    element.on("keyup", function(e) {
-      if (e.keyCode == 13 && !e.shiftKey && !e.ctrlKey && !e.altKey) {
-        e.preventDefault();
-        scope.$apply(function() {
-          scope.$emit("sendmessage");
-        });
-      }
-    });
-    element.on("keydown", function(e) {
-      if (e.keyCode == 13 && !e.shiftKey && !e.ctrlKey && !e.altKey) {
-        e.preventDefault();
-      }
-    });
-  }
-
-  return {
-    //scope: { onSendMessage: '&' },
-    link: link
-  };
-}])
-
-// maybe replace with https://github.com/Luegg/angularjs-scroll-glue/blob/master/src/scrollglue.js#L62
-.directive('scrollDown', ['$interval',function($interval) {
-  return {
-    link: function link(scope, $element, attrs) {
-      var element = $element[0];
-      function scroll() {
-        //console.log("scroll",element);
-        element.scrollTop = element.scrollHeight;
-      }
-      scope.$watch(function() {
-        //console.log(element.scrollHeight, element.scrollTop)
-        if (element.scrollHeight - element.scrollTop - element.clientHeight < 400)
-          scroll();
-
-        //else
-        //  console.log("scroll-down: not scrolling down because too far scrolled up");
-      });
-      $interval(scroll, 1, 1);
-      $element.on("scroll", function() {
-        if (element.scrollTop == 0) {
-          scope.$apply(function() {
-            scope.$emit("reachedtop");
-            element.scrollTop = 20;
-          });
-        }
-      });
-    }
-  };
-}])
 
 
 ;
