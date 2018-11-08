1db4c159ea8d65db576f107d5ceffc86ef58bf9c
Max Weller <max@teamwiki.de>
2018-11-08 20:23:42 +0100

    extract config to separate file

---
+++ client/config_template.js
@@ -0,0 +1,6 @@
+
+// config
+XMPP_WEBSOCKET_SERVICE = 'wss://teamwiki.de:5281/xmpp-websocket/';
+XMPP_ALLOWED_HOSTS = ['teamwiki.de'];
+XMPP_REGISTER_FORM = 'https://teamwiki.de:5281/register_web';
+FILE_UPLOAD_API_ENDPOINT = 'https://chat2.teamwiki.de';
